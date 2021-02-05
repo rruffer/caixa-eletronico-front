cd61f615e9e5b8a727c0d131c7a64313888785ee
rruffer <Devchine@15>
2021-02-05 13:07:03 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { FooterComponent } from './pages/main/footer/footer.component';
+import { HeaderComponent } from './pages/main/header/header.component';
+import { MainComponent } from './pages/main/main.component';
+import { CadastroBeneficiarioComponent } from './pages/views/cadastro-beneficiario/cadastro-beneficiario.component';
+import { MaterialModule } from './modules/material/material.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CadastroBeneficiarioComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/pages/views/cadastro-beneficiario/cadastro-beneficiario.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastro-beneficiario',
+  templateUrl: './cadastro-beneficiario.component.html',
+  styleUrls: ['./cadastro-beneficiario.component.scss']
+})
+export class CadastroBeneficiarioComponent implements OnInit {
+
+  form: FormGroup;
+
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+
+    this.form = this.formBuilder.group({
+      nome: [null, { validators: [Validators.required], updateOn: "change" }],
+      cpf: [null, { validators: [Validators.required], updateOn: "change" }],
+      email: [null, { validators: [Validators.required, Validators.email], updateOn: "change" }],
+      totalSaldoAposentadoria: [null, { validators: [Validators.required], updateOn: "change" }],
+      qtdAnosAposentadoria: [null, { validators: [Validators.required], updateOn: "change" }],
+    });
+
+  }
+
+  onSubmit(): void {
+
+    if (this.form.valid) {
+
+    } else {
+      alert('erro form');
+    }
+
+  }
+
+}
